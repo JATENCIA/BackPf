c6be35b2c8567bf8d3eafef253b7b34c9bd4f17d
JATENCIA <atenciajaramilloj@gmail.com>
2023-02-09 18:22:57 -0500

    update

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-process.env.MONGOHOST;
-process.env.MONGOPASSWORD;
-process.env.MONGOPORT;
-process.env.MONGOUSER;
-
-const MONGO_URL = `mongodb://MONGOUSER:MONGOPASSWORD@containers-us-west-147.railway.app:6604`;
-
 const MONGODB = () => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect(MONGO_URL)
+    .connect(process.env.MONGO_URL)
     .then(() => console.log("Connected to MongoDB-URL"))
     .catch((error) =>
       console.error("Error connection to MongoDB-URL", error.message)
