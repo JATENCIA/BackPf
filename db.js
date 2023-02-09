a33b8b50b0908b308db30c7bbd1cc49df924352d
JATENCIA <atenciajaramilloj@gmail.com>
2023-02-09 17:34:26 -0500

    update

--- db.js
+++ db.js
@@ -12,7 +12,9 @@ const MONGO_URL =
 const MONGODB = () => {
   mongoose.set("strictQuery", true);
   mongoose
-    .connect(MONGO_URL)
+    .connect(
+      "mongodb://mongo:F4nsQEwDCvLV3Cbovcqq@containers-us-west-147.railway.app:6604"
+    )
     .then(() => console.log("Connected to MongoDB-URL"))
     .catch((error) =>
       console.error("Error connection to MongoDB-URL", error.message)
